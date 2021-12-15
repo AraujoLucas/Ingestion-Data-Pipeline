39c3cd29e438d597c6b72ade44234323fca7aab5
Lucas Araujo <lucasaraujo@MacBook-Air-de-Lucas.local>
2021-12-14 21:41:29 -0300

    ensinando a gabi fazer terraform

--- infraestructure.tf
+++ infraestructure.tf
@@ -5,7 +5,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 # ----- Camada de armazenamento -----#
 #Raw 
 resource "aws_s3_bucket" "bucket_1" {
@@ -17,16 +16,7 @@ resource "aws_s3_bucket" "bucket_1" {
     Environment = "Dev"
   }
 }
-#Analytics 
-resource "aws_s3_bucket" "bucket_2" {
-  bucket = "corp-analytics-684264620210"
-  acl    = "private"
 
-  tags = {
-    Name        = "bucket-analytics"
-    Environment = "Dev"
-  }
-}
 #Artifacts
 resource "aws_s3_bucket" "bucket_3" {
   bucket = "artifacts-684264620210"
@@ -37,49 +27,15 @@ resource "aws_s3_bucket" "bucket_3" {
     Environment = "Dev"
   }
 }
-#Results-work-group-analytics
-resource "aws_s3_bucket" "bucket_4" {
-  bucket = "results-work-group-analytics-684264620210"
-  acl    = "private"
-
-  tags = {
-    Name        = "bucket-results"
-    Environment = "Dev"
-  }
-}
 
 # ----- Camada de ingestao -----#
 #Generate
 resource "aws_glue_job" "job_ingestion" {
+  glue_version  = "2.0"
   name     = "data_generate"
-  role_arn = "arn:aws:iam::587791419323:role/AraujoEngineer"
+  role_arn = "arn:aws:iam::684264620210:role/datalake_devopsrole"
 
   command {
     script_location = "s3://artifacts/example.py"
-  }
-}
-
-# ----- Camada de processamento -----#
-#Aggregator
-resource "aws_glue_job" "job_transformer" {
-  name     = "data_aggregator"
-  role_arn = "arn:aws:iam::587791419323:role/AraujoEngineer"
-
-  command {
-    script_location = "s3://artifacts/example.py"
-  }
-}
-
-# ----- Camada de visualizacao -----#
-#Work-group-Analytics
-resource "aws_athena_workgroup" "work-group" {
-  name = "Work-group-analytics"
-
-  configuration {
-    enforce_workgroup_configuration    = true
-
-    result_configuration {
-      output_location = "s3://results-work-group-analytics-684264620210/output/"
-    }
   }
 }
